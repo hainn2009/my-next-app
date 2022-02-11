5f9af6d16d5da508c31fa7c541eeca1e232fd740
Nguyen Ngoc Hai <hainn2009@gmail.com>
2022-02-11 21:43:25 +0700

    event-item

--- components/events/event-item.js
+++ components/events/event-item.js
@@ -1,7 +1,31 @@
-export default function EventItem({ item }) {
+import Link from "next/link";
+
+export default function EventItem(props) {
+  const { title, image, date, location, id } = props;
+
+  const humanReadableDate = new Date(date).toLocaleDateString("vi-VN", {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+  });
+  const formattedAddress = location.replace(", ", "\n");
   return (
     <li>
-      Id: {item.id}, Title: {item.title}
+      <imb src='' alt='' />
+      <div>
+        <div>
+          <h2>{title}</h2>
+          <div>
+            <time>{humanReadableDate}</time>
+          </div>
+          <div>
+            <address>{formattedAddress}</address>
+          </div>
+        </div>
+        <div>
+          <Link href='/'>Exporing Event</Link>
+        </div>
+      </div>
     </li>
   );
 }
