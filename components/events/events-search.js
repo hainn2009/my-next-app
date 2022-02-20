b1e1314b52489a86fa2cf797141362437e292f4c
Nguyen Ngoc Hai <hainn2009@gmail.com>
2022-02-20 20:03:56 +0700

    Update page filterEvent

--- components/events/events-search.js
+++ components/events/events-search.js
@@ -19,25 +19,25 @@ export default function EventsSearch(props) {
         <div className={classes.control}>
           <label htmlFor='year'>Year</label>
           <select id='year' ref={yearInputRef}>
-            <option key='2021'>2021</option>
-            <option key='2022'>2022</option>
+            <option value='2021'>2021</option>
+            <option value='2022'>2022</option>
           </select>
         </div>
         <div className={classes.control}>
           <label htmlFor='month'>Month</label>
           <select id='month' ref={monthInputRef}>
-            <option key='1'>January</option>
-            <option key='2'>February</option>
-            <option key='3'>March</option>
-            <option key='4'>April</option>
-            <option key='5'>May</option>
-            <option key='6'>June</option>
-            <option key='7'>July</option>
-            <option key='8'>August</option>
-            <option key='9'>September</option>
-            <option key='10'>October</option>
-            <option key='11'>November</option>
-            <option key='12'>December</option>
+            <option value='1'>January</option>
+            <option value='2'>February</option>
+            <option value='3'>March</option>
+            <option value='4'>April</option>
+            <option value='5'>May</option>
+            <option value='6'>June</option>
+            <option value='7'>July</option>
+            <option value='8'>August</option>
+            <option value='9'>September</option>
+            <option value='10'>October</option>
+            <option value='11'>November</option>
+            <option value='12'>December</option>
           </select>
         </div>
       </div>
--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -1,7 +1,31 @@
 import { useRouter } from "next/router";
+import { getFilteredEvents } from "../../dummy-data";
 
 export default function FilteredEventsPage() {
   const router = useRouter();
+  const filterData = router.query.slug;
+
+  if (!filterData) return <p className='center'>Loading...</p>;
+  const year = +filterData[0];
+  const month = +filterData[1];
+  console.log(year);
+  console.log(month);
+  if (
+    isNaN(year) ||
+    isNaN(month) ||
+    year > 2030 ||
+    year < 2021 ||
+    month < 1 ||
+    month > 12
+  ) {
+    return <p>Invalid Filter. Please adjust your values!</p>;
+  }
+
+  const filterEvents = getFilteredEvents({ year, month });
+  if (!filterEvents || filterEvents.length === 0) {
+    return <p>No event found with your chosen filter!</p>;
+  }
+
   console.log(router.query);
   return (
     <div>
