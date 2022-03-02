48d7921a5b6c575e7672a865c9883e2f0fc5ced1
Nguyen Ngoc Hai <hainn2009@gmail.com>
2022-03-02 08:57:44 +0700

    Add serverside rendering

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,22 +1,57 @@
-import { Fragment } from "react";
+import { Fragment, useEffect, useState } from "react";
 import { useRouter } from "next/router";
+import useSWR from "swr";
 
 import { getAllEvents } from "../../dummy-data";
 import EventList from "../../components/events/event-list";
 import EventsSearch from "../../components/events/events-search";
 
-export default function AllEventsPage() {
-  const router = useRouter();
+const fetcher = (...args) => fetch(...args).then((res) => res.json());
+export default function AllEventsPage(props) {
+  const [events, setEvents] = useState(props.events);
+  const { data, error } = useSWR(
+    "https://my-events-project-19872-default-rtdb.asia-southeast1.firebasedatabase.app/events.json",
+    fetcher
+  );
+  useEffect(() => {
+    if (data) {
+      console.log("data", data);
+      const transformEvents = Object.keys(data).map((id) => ({
+        id: id,
+        ...data[id],
+      }));
+      setEvents(transformEvents);
+    }
+  }, [data]);
 
+  const router = useRouter();
   function findEventHandler(year, month) {
     const fullPath = `/events/${year}/${month}`;
     router.push(fullPath);
   }
+  if (error) return <p>No data yet</p>;
+  if (!events) return <p>Loading...</p>;
+  if (events) {
+    console.log(events);
 
-  return (
-    <Fragment>
-      <EventsSearch onSearch={findEventHandler} />
-      <EventList items={getAllEvents()} />
-    </Fragment>
+    return (
+      <Fragment>
+        <EventsSearch onSearch={findEventHandler} />
+        {/* <EventList items={getAllEvents()} /> */}
+        <EventList items={events} />
+      </Fragment>
+    );
+  }
+}
+
+export async function getStaticProps() {
+  const response = await fetch(
+    "https://my-events-project-19872-default-rtdb.asia-southeast1.firebasedatabase.app/events/e1.json"
   );
+  const events = await response.json();
+  return {
+    props: {
+      events: [{ id: "e1", ...events }],
+    },
+  };
 }
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,27 @@
 import { getFeaturedEvents } from "../dummy-data";
 import EventList from "../components/events/event-list";
 
-export default function HomePage() {
+export default function HomePage({ events }) {
   return (
     <div>
       <h1>Featured Events</h1>
-      <EventList items={getFeaturedEvents()} />
+      {/* <EventList items={getFeaturedEvents()} /> */}
+      <EventList items={events} />
     </div>
   );
+}
+
+export async function getStaticProps() {
+  const response = await fetch(
+    "https://my-events-project-19872-default-rtdb.asia-southeast1.firebasedatabase.app/events.json"
+  );
+  const events = await response.json();
+
+  return {
+    props: {
+      events: Object.keys(events)
+        .map((id) => ({ ...events[id], id: id }))
+        .filter((e) => e.isFeatured),
+    },
+  };
 }
