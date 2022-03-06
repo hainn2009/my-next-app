d8e785c48b74c3310e09e34f2286e74cf645888c
Nguyen Ngoc Hai <hainn2009@gmail.com>
2022-03-06 22:32:03 +0700

    Add newsletter

--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -1,11 +1,24 @@
+import { MongoClient } from "mongodb";
 import { saveNewletterRegistration } from "../../helpers/newsletter-api-util";
 
-export default function handler(req, res) {
+export default async function handler(req, res) {
   const email = req.body.email;
   // will get error if pass email as a number
   if (!email || email.trim() === "" || !email.includes("@"))
     return res.status(422).json({ message: "Invalid email address" });
-  saveNewletterRegistration(email);
+  const client = await MongoClient.connect(
+    "mongodb+srv://newuser:Dp7Q17bm2IGQkSbu@cluster0.5imn6.mongodb.net/newsletter?retryWrites=true&w=majority"
+  );
 
-  res.status(201).json({ email });
+  const db = client.db();
+  const result = await db.collection("emails").insertOne({ email });
+  client.close();
+  // saveNewletterRegistration(email);
+
+  res
+    .status(201)
+    .json({
+      message: "Sign up",
+      data: { id: result.insertedId.toString(), email },
+    });
 }
